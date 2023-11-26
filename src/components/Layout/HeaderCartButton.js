5b3b37470263cca05f39fac76e8cdbffdebb94b7
alisalali <alis.alali@hotmail.com>
2023-11-26 22:05:13 +0300

    working with ref and Froward ref with context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -6,9 +6,11 @@ import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
-    return currNumber + item.amount;
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
   }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
